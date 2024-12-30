6418f5919ecdb48947d3f32bd6c8f16e003c5284
Yao Zu <1796381757@qq.com>
2024-12-30 22:07:53 +0800

    添加主题样式文件并在布局组件中测试全局和局部样式

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import sum from "@/test";
 import router from "@/router";
 
+// 导入定制主题文件
+import "./theme.css";
+
 const total = sum(1, 2);
 console.log(total);
 
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom";
+import { Button } from "antd-mobile";
 
 const Layout = () => {
   return (
     <div>
       <Outlet />
       我是 Layout 组件
+      {/* 测试全局生效样式 */}
+      <Button color="primary">测试全局</Button>
+      <div className="purple">
+        {/* 测试局部生效样式 */}
+        <Button color="primary">测试局部</Button>
+      </div>
     </div>
   );
 };
