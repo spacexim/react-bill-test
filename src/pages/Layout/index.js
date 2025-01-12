cd1e5bc90011cb0955ff9bb5619ab11967d2d7cf
Yao Zu <1796381757@qq.com>
2025-01-12 20:16:16 +0800

    添加布局样式，集成月度账单页面，使用 lodash 进行数据分组，增加日期选择功能

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,23 +1,58 @@
-import { Outlet } from "react-router-dom";
-import { Button } from "antd-mobile";
+import { TabBar } from "antd-mobile";
 import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getBillList } from "@/store/modules/billStore";
+import "./index.scss";
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
+
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+]
 
 const Layout = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(getBillList());
-  }, [dispatch]);
+    dispatch(getBillList())
+  }, [dispatch])
+
+  const navigate = useNavigate()
+
+  const switchRoute = (path) => {
+    console.log(path)
+    navigate(path)
+  }
+
   return (
-    <div>
-      <Outlet />
-      我是 Layout 组件
-      {/* 测试全局生效样式 */}
-      <Button color="primary">测试全局</Button>
-      <div className="purple">
-        {/* 测试局部生效样式 */}
-        <Button color="primary">测试局部</Button>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+
+      <div className="footer">
+        <TabBar onChange={switchRoute}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
       </div>
     </div>
   );
--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,5 +1,93 @@
-const Month = () => {
-  return <div>我是 Month 组件</div>;
-};
+import { NavBar, DatePicker } from 'antd-mobile'
+import './index.scss'
+import { useMemo, useState } from 'react'
+import classNames from 'classnames'
+import dayjs from 'dayjs'
+import { useSelector } from 'react-redux'
+import _, { set } from 'lodash'
 
-export default Month;
+const Month = () => {
+  // 按月做数据的分组
+  const billList = useSelector(state => state.bill.billList)
+  const monthGroup = useMemo(() => {
+    // return 出去计算之后的值
+    return _.groupBy(billList, (item) => dayjs(item.date).format('YYYY-MM'))
+  }, [billList])
+  console.log(monthGroup)
+
+  // 控制弹框打开和关闭
+  const [dateVisible, setDateVisible] = useState(false)
+
+  // 控制时间显示
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs(new Date()).format('YYYY-MM')
+  })
+
+  const [currentMonthList, setMonthList] = useState([])
+
+  const monthResult = useMemo(() => {
+    const pay = currentMonthList.filter(item => item.type === 'pay').reduce((a, c) => a + c.money, 0)
+    const income = currentMonthList.filter(item => item.type === 'income').reduce((a, c) => a + c.money, 0)
+    return {
+      pay,
+      income,
+      total: pay + income
+    }
+
+  }, [currentMonthList])
+  // 确认回调
+  const onConfirm = (date) => {
+    setDateVisible(false)
+    console.log(date)
+    const formatDate = dayjs(date).format('YYYY-MM')
+    setMonthList(monthGroup[formatDate])
+    setCurrentDate(formatDate)
+  }
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={() => setDateVisible(true)}>
+            <span className="text">
+              {currentDate + ''}月账单
+            </span>
+            {/* 思路：根据当前弹框的状态控制expand类名是否存在 */}
+            <span className={classNames('arrow', dateVisible && 'expand')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className="item">
+              <span className="money">{monthResult.pay.toFixed(2)}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{monthResult.income.toFixed(2)}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{monthResult.total.toFixed(2)}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dateVisible}
+            onCancel={() => { setDateVisible(false) }}
+            onConfirm={onConfirm}
+            onClose={() => { setDateVisible(false) }}
+            max={new Date()}
+          />
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default Month
